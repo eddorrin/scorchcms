aa953d3c2d384cbfda6daf0877b7bd69bb4c8af5
Ed Dorrin <eddorrin@mail.com>
2016-06-07 08:23:30 +0200

    Update config.php

--- dev/includes/config.php
+++ dev/includes/config.php
@@ -11,13 +11,13 @@
     
     /* Database connection */
     $_CONF['data']['host'] = "localhost"; //Usually localhost. Leave as is unless it doesn't work!
-    $_CONF['data']['user'] = "ispecws"; //Your database user, most use 'root' anyway.
-    $_CONF['data']['pass'] = "1o7Gfour10"; //Password for user, duh! :)
-    $_CONF['data']['name'] = "ispecws_scms"; //Now to name your database.. You uploaded a database right?
+    $_CONF['data']['user'] = "root"; //Your database user, most use 'root' anyway.
+    $_CONF['data']['pass'] = ""; //Password for user, duh! :)
+    $_CONF['data']['name'] = "scorch"; //Now to name your database.. You uploaded a database right?
     
     /* Website setup */
     $_CONF['web']['name'] = "Scorch"; //The name of your hotel.
-    $_CONF['web']['link'] = "http://se-gm.com/prj/sCMS"; //Link your hotel. No ending slash (/)!
+    $_CONF['web']['link'] = "http://localhost"; //Link your hotel. No ending slash (/)!
     $_CONF['web']['desc'] = "Best hotel ever!"; //Short description of hotel
     $_CONF['web']['meta'] = "best;hotel;ever;retro;scorch;fun;happy;entertaining;game;"; //Hotel tags for easier search functionalities.
     $_CONF['web']['logo'] = "includes/Scorch.gif"; //Custom Logo
